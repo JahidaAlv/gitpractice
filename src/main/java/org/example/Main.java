0f9b21867f126a83cba783882c1a12fb5ea50ca1
jahida <alijahida128@gmail.com>
2025-04-11 19:08:33 +0530

    k

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,6 +8,7 @@ public class Main {
         System.out.println("Hello, Jahida");
         System.out.println("Jahida is coding");
         System.out.println("Power");
+        System.out.println("Kuch bhi");
 
 
 
