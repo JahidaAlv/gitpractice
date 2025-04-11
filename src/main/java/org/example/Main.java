73efaa97bfbbcf1f4c517631ada6bcc2bf655e19
jahida <alijahida128@gmail.com>
2025-04-11 22:26:47 +0530

    name

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,12 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Jahida alv");
 
-        System.out.println("Jahida is coding");
-        System.out.println("Power");
-        System.out.println("New Code Added");
-        System.out.println("New Code Added");
-        System.out.println("New Code Added");
-        System.out.println("New Code Added");
     }
 }
