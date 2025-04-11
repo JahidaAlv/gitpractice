3359ed870e1b5049f6b6ebeae162a1db1fde26c8
JahidaAlv <alijahida148@gmail.com>
2025-04-11 19:07:09 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,6 +8,8 @@ public class Main {
         System.out.println("Hello, Jahida");
         System.out.println("Jahida is coding");
         System.out.println("Power");
+        System.out.println("New Code Added");
+
 
 
 
