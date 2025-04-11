479ddb8cce9b8992745c7fb76e9be07cafb75da4
jahida <alijahida128@gmail.com>
2025-04-11 19:14:19 +0530

    d

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,9 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
-        System.out.println("Hello, Powerhouse!");
-        System.out.println("Hello, Jahida");
+
         System.out.println("Jahida is coding");
         System.out.println("Power");
 
